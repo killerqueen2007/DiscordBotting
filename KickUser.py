627583b1fb1fdc3a09dff81c4d34979dd4be6034
killerqueen2007 <adendaniels0910@gmail.com>
2026-08-03 17:50:10 -0400

    Add files via upload

---
+++ KickUser.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import random
+
+Guild_ID = ""
+User_ID= ""
+
+def send_command():
+    url = f"https://discord.com/api/v9/guilds/{Guild_ID}/members/{User_ID}?reason="
+    headers = {
+        "Authorization": "DISCORD TOKEN",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.delete(url, headers=headers)
+    print(response.status_code)
+
+send_command()
